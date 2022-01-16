6ac91555a9a9be41910afde062c37ead2203e394
foralost <kombii4@gmail.com>
2022-01-17 00:44:07 +0100

    CRC32: Working reflected form of polynomial

--- src/include/crc.c
+++ src/include/crc.c
@@ -88,13 +88,8 @@ uint64_t crc_calc_table(char *data, size_t length, struct crc_info *info,
 	__crc_calc_tab_init(tab->bytes_per_iter, info);
 
 	uint16_t top_short = 0;
-	uint64_t reg = info->init_val;
 	uint64_t reg_mask = __crc_gen_mask(info->pol_width * 8);
-
-	if (info->init_val)
-		for (uint8_t i = 0; i < info->pol_width * 8; i++)
-			reg = reg & 1 ?
-					 ((uint64_t)(reg ^ info->polynomial) >> (uint64_t)1 | (uint64_t)reg_mask ) : (reg >> (uint64_t)1);
+	uint64_t reg = __crc_gen_init_reg(info, &reg_mask);
 
 	for (size_t i = 0; i < length;) {
 		if (info->ref_form) {
@@ -149,20 +144,46 @@ uint64_t crc_calc_table(char *data, size_t length, struct crc_info *info,
 	return reg;
 }
 
-uint64_t crc_calc(char *data, size_t length, struct crc_info *info) {
-// TODO: That black magic with inversed CRC32 when init_val != 0
-	uint64_t reg;
-	uint64_t reg_mask = __crc_gen_mask(info->pol_width * 8);
+uint64_t __crc_gen_init_reg(struct crc_info *info, uint64_t *reg_mask) {
 
-	if (info->init_val){
+	uint64_t reg = 0;
+	if (info->init_val) {
 		reg = info->init_val;
-		for (uint8_t i = 0; i < info->pol_width * 8; i++)
-			reg = reg & 1 ?
-					 ((uint64_t)(reg ^ info->polynomial) >> (uint64_t)1 | reg_mask ) : (reg >> (uint64_t)1);
-	}else{
+		if (!info->ref_form) {
+			for (uint8_t i = 0; i < (info->pol_width << 3); i++)
+				reg = reg & 1 ?
+						(uint64_t) (reg ^ info->polynomial) >> (uint64_t) 1
+								| *reg_mask :
+						(reg >> (uint64_t) 1);
+		} else {
+
+			uint64_t temp = 0;
+			for (uint64_t i = 0; i < (info->pol_width << 3); i++)
+				if (reg & ((uint64_t) 1 << i))
+					temp |= (uint64_t) 1 << ((info->pol_width << 3) - i - 1);
+
+			reg &= ~__crc_gen_and_mask(info->pol_width << 3);
+			reg |= temp;
+
+			for (uint8_t i = 0; i < (info->pol_width << 3); i++)
+				reg = reg & *reg_mask ?
+						(uint64_t) (reg ^ info->polynomial) << (uint64_t) 1
+								| 1 :
+						(reg << (uint64_t) 1);
+		}
+	} else {
 		reg = 0;
 	}
 
+	return reg;
+
+}
+uint64_t crc_calc(char *data, size_t length, struct crc_info *info) {
+// TODO: That black magic with inversed CRC32 when init_val != 0
+	uint64_t reg;
+	uint64_t reg_mask = __crc_gen_mask(info->pol_width << 3);
+
+	reg = __crc_gen_init_reg(info, &reg_mask);
 	for (size_t i = 0; i < length; i++) {
 		for (uint8_t j = 0; j < 8; j++) {
 			uint8_t curr_bit;
@@ -172,7 +193,8 @@ uint64_t crc_calc(char *data, size_t length, struct crc_info *info) {
 				curr_bit = (data[i] & (1 << (7 - j))) ? 1 : 0;
 
 			if (info->ref_form) {
-				uint64_t app_val = (uint64_t)curr_bit << (uint64_t)((info->pol_width << 3) - 1);
+				uint64_t app_val = (uint64_t) curr_bit
+						<< (uint64_t) ((info->pol_width << 3) - 1);
 				reg = (reg & 1) ?
 						((reg >> 1) | app_val) ^ info->polynomial :
 						((reg >> 1) | app_val);
@@ -191,7 +213,7 @@ uint64_t crc_calc(char *data, size_t length, struct crc_info *info) {
 			reg = (reg & reg_mask) ? (reg << 1) ^ info->polynomial : (reg << 1);
 		}
 	}
-
+	printf("%x\n", __crc_inv_byte(0xb6));
 	__crc_final_round(&reg, info);
 	return reg;
 }
--- src/main.c
+++ src/main.c
@@ -16,13 +16,24 @@ int main(int argc, char** argv)
 	crc.ref_in = 0;
 	crc.ref_out = 0;
 	crc.xor_val = -1;
-	crc.init_val = 0x00000000ffffffff;
+	crc.init_val = (uint32_t)0xFFFFFFFF;
 	crc.ref_form = 1;
 
-	char* test = "123456789";
+	char* test = "1";
 	size_t s = strlen(test);
 
-	printf("%lx\n", crc_calc(test, s, &crc));
+	uint64_t calc_val = crc_calc(test, s, &crc);
+	printf("%lx\n", calc_val);
+
+	uint64_t rev = 0;
+	for(uint64_t i = 0 ; i < 64;i++){
+		uint64_t rev_mask = ( (uint64_t) 1 << ( 63 - i ));
+		if( calc_val & ((uint64_t) 1 << i) ){
+			rev |= rev_mask;
+		}
+	}
+
+
 
 
 	return 0;
